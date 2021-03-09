4a9101da95e870f00970013726e24938ef5664a8
Niklas Vejlby Schjoldager <nikl1565@stud.kea.dk>
2021-03-09 17:57:08 +0100

    Changed animation to transition, to fix animation bug

--- script.js
+++ script.js
@@ -24,12 +24,17 @@ function toggleOption(event) {
     // Toggle feature in "model"
     features[feature] = !features[feature];
 
+    const doesFeatureElementAlreadyExist = document.querySelector(`#selected ul li[data-feature=${feature}]`);
+    console.log(doesFeatureElementAlreadyExist);
+
     // If feature is (now) turned on:
     if (features[feature]) {
         // feature added
         console.log(`Feature ${feature} is turned on!`);
+
         // - mark target as chosen (add class "chosen")
         target.classList.add("chosen");
+
         // - un-hide the feature-layer(s) in the #product-preview;
         // Get all feature elements (e.g. sun shield has 2 images)
         const featureElements = document.querySelectorAll(`img[data-feature=${feature}]`);
@@ -38,11 +43,41 @@ function toggleOption(event) {
         featureElements.forEach((featureElement) => {
             featureElement.classList.remove("hide");
         });
-        // - create featureElement and append to #selected ul
-        // - create FLIP-animation to animate featureElement from img in target, to
-        //   its intended position. Do it with normal animation or transition class!
 
-        // TODO: More code
+        if (doesFeatureElementAlreadyExist) {
+            // doesFeatureElementAlreadyExist.offsetHeight;
+            doesFeatureElementAlreadyExist.classList.add("animate-feature");
+            doesFeatureElementAlreadyExist.removeEventListener("transitionend", elementTransitionEnd);
+        } else {
+            // - create featureElement and append to #selected ul
+            const featureElement = createFeatureElement(feature);
+            // Used so the transition is instant to the top
+            // where we want it to begin with, so the animation starts
+            // from the start
+            featureElement.style.transition = "none";
+            document.querySelector("#selected ul").append(featureElement);
+
+            // - create FLIP-animation to animate featureElement from img in target, to
+            //   its intended position. Do it with normal animation or transition class!
+            // 1. first: find the start-position
+            const start = target.getBoundingClientRect();
+            // 2. last: find the end-position
+            const end = featureElement.getBoundingClientRect();
+            // 3. invert: translate the element to the start-position
+            const diffX = start.x - end.x;
+            const diffY = start.y - end.y;
+
+            featureElement.style.setProperty("--diffX", diffX);
+            featureElement.style.setProperty("--diffY", diffY);
+
+            // featureElement.offsetHeight; // reflow layout - necessary for Firefox ...
+            // 4. play: animate the element to translate(0,0)
+            featureElement.offsetHeight; // reflow layout to position it at the feature button instantly
+            // Make the transition smooth again
+            featureElement.style.transition = "";
+            // Then run the animation
+            featureElement.classList.add("animate-feature");
+        }
     } else {
         // Else - if the feature (became) turned off:
         // feature removed
@@ -50,6 +85,7 @@ function toggleOption(event) {
 
         // - no longer mark target as chosen
         target.classList.remove("chosen");
+
         // - hide the feature-layer(s) in the #product-preview
         // Get all feature elements (e.g. sun shield has 2 images)
         const featureElements = document.querySelectorAll(`img[data-feature=${feature}]`);
@@ -58,12 +94,24 @@ function toggleOption(event) {
         featureElements.forEach((featureElement) => {
             featureElement.classList.add("hide");
         });
+
         // - find the existing featureElement in #selected ul
+        const featureElement = document.querySelector(`#selected ul li[data-feature=${feature}]`);
+
         // - create FLIP-animation to animate featureElement to img in target
+        // featureElement.classList.remove("animate-feature-in");
+        // featureElement.classList.add("animate-feature-out");
+        featureElement.classList.remove("animate-feature");
+
         // - when animation is complete, remove featureElement from the DOM
+        featureElement.addEventListener("transitionend", elementTransitionEnd);
 
         // TODO: More code
     }
+}
+
+function elementTransitionEnd() {
+    this.remove();
 }
 
 // Create featureElement to be appended to #selected ul - could have used a <template> instead
